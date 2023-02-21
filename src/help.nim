8a51303d21e3995d26c1c1b86bc54c681a03455a
dylanagreen <dylan@mygreenfamily.us>
2023-02-20 17:46:14 -0800

    Move about help into a file.
    - The goal is to write all help pages in files that are loaded at compile time.

--- src/help.nim
+++ src/help.nim
@@ -1,15 +1,17 @@
+import os
 import strutils
 import tables
 
 let
-  help_table = {"about": "NAME: about \nUSAGE: about \nDESCRIPTION: Prints information about the program.",
+  help_table = {"about": readFile(getAppDir()  / "help_pages" / "about.txt"),
                 "help": "NAME: help \nUSAGE: help [COMMAND] \nDESCRIPTION: Prints help information about COMMAND."}.toTable
 
 proc help_string*(cmd: string) =
+  echo help_table
   let subs = cmd.split(" ")
   var key = "help" # By default show the help string for help command
 
-  if subs.len > 2:
+  if subs.len >= 2:
     key = subs[1]
 
   echo help_table[key]
